4ad27ed74a31b7879c2ee23b4f19af379c921abb
itmam <itmamalam40@gmail.com>
2024-12-18 09:24:07 +0100

    rofi: remove confirm dialog

--- rofi/.config/rofi/powermenu/type-3/powermenu.sh
+++ rofi/.config/rofi/powermenu/type-3/powermenu.sh
@@ -54,7 +54,7 @@ run_rofi() {
 
 # Execute Command
 run_cmd() {
-	# selected="$(confirm_exit)"
+        selected="$yes" #"$(confirm_exit)"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
 			systemctl poweroff
